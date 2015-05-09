7c14ee7116e367f2320a7873f9bb32953b9cb185
Paul Aglione <aglionep@gmail.com>
2015-05-08 17:14:59 -0700

    second commit

--- app.js
+++ app.js
@@ -1,23 +1,28 @@
-var boxes = document.getElementsByClassName("box)");
+var boxes = document.getElementsByClassName("box");
 console.log(boxes);
 
 var player = true;
 
+window.addEventListener("load", function() {
+	inputPlayers();
+});
+
 inputPlayers();
 
 function inputPlayers () {
-for(var i = 0; i<boxes.lenght; i++){
-				console.log("loop called");
-				boxes[i].addeventlistener('click', function(){
-					console.log("x");
-					if(player ===true && this.innerHTML === ""){
-					this.innerHTML = "x"; player = false;
-				}	else if(player===false && this.innerHTML ===""){
-					this.innerHTML = "o"; player = true;
-				} 	else {
-					alert("Choose an empty block);");
-				}
+	console.log("start function");
+	for(var i = 0; i < boxes.length; i++){
+		console.log("loop called");
+		boxes[i].addEventListener('click', function(){
+			console.log("x");
+			if(player ===true && this.innerHTML === ""){
+				this.innerHTML = "x"; player = false;
+			}	else if(player===false && this.innerHTML ===""){
+				this.innerHTML = "o"; player = true;
+			} 	else {
+				// alert("Choose an empty block");
+			}
 		}
-	);
-}
+		);
+	}
 }
