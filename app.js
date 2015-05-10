39144308e0fefd039bbccf5369c202d3c98552b3
Paul Aglione <aglionep@gmail.com>
2015-05-09 22:13:16 -0700

    x and o images

--- app.js
+++ app.js
@@ -16,9 +16,9 @@ function inputPlayers () {
 		boxes[i].addEventListener('click', function(){
 			console.log("x");
 			if(player ===true && this.innerHTML === ""){
-				this.innerHTML = "x"; player = false;
+				this.innerHTML = "<img src=images/X.png>"; player = false;
 			}	else if(player===false && this.innerHTML ===""){
-				this.innerHTML = "o"; player = true;
+				this.innerHTML = "<img src=images/O.png>"; player = true;
 			} 	else {
 				// alert("Choose an empty block");
 			}
@@ -26,3 +26,12 @@ function inputPlayers () {
 		);
 	}
 }
+function getWinner() {
+	if (isWinner('x')) {
+		return 'x';
+	}
+	if (isWinner('o')) {
+		return 'o';
+	}
+	return null;
+};
